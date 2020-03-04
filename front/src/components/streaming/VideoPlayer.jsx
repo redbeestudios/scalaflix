cab6325d42b639ff6b3add1a334989d5637c53cf
Ariel Felipe Camacho Diaz <ariel.camacho@redb.ee>
2020-03-04 19:07:40 -0300

    Adjust width of Video player

--- front/src/components/streaming/VideoPlayer.jsx
+++ front/src/components/streaming/VideoPlayer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Player} from 'video-react';
+import "../../../node_modules/video-react/dist/video-react.css";
 import './VideoPlayer.css';
 import Grid from "@material-ui/core/Grid";
 import CloseIcon from '@material-ui/icons/Close';
