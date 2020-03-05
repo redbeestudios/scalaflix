653c8343e3d76db5ee49533ce6389d45f52c9ef4
Santiago Federella <sfederella-rb@github.com>
2020-03-05 15:17:19 -0300

    Change sql date to LocalDateTime

--- services/streaming/app/repositories/tables/FilmTable.scala
+++ services/streaming/app/repositories/tables/FilmTable.scala
@@ -1,18 +1,18 @@
 package repositories.tables
 
-import java.sql.Date
+import java.time.LocalDateTime
 
 import domain.Film
 import slick.jdbc.PostgresProfile.api._
 import slick.lifted.{ProvenShape, TableQuery}
 
-class FilmTable(tag: Tag) extends Table[Film](tag, "films") with SqlUtils {
+class FilmTable(tag: Tag) extends Table[Film](tag, "films") {
 
   def id: Rep[Int]             = column[Int]("id", O.PrimaryKey, O.AutoInc)
   def name: Rep[String]        = column[String]("name")
   def description: Rep[String] = column[String]("description")
   def duration: Rep[Int]       = column[Int]("duration")
-  def uploadDate: Rep[Date]    = column[Date]("uploadDate")
+  def uploadDate: Rep[LocalDateTime] = column[LocalDateTime]("uploadDate")
   def views: Rep[Long]         = column[Long]("views")
   def available: Rep[Boolean]  = column[Boolean]("available")
 
@@ -26,7 +26,7 @@ class FilmTable(tag: Tag) extends Table[Film](tag, "films") with SqlUtils {
             description = description,
             genres = Nil,
             duration = duration,
-            uploadDate = toLocalDateTime(uploadDate),
+            uploadDate = uploadDate,
             views = views,
             available = available
           )
@@ -37,7 +37,7 @@ class FilmTable(tag: Tag) extends Table[Film](tag, "films") with SqlUtils {
             film.name,
             film.description,
             film.duration,
-            toSqlDate(film.uploadDate),
+            film.uploadDate,
             film.views,
             film.available
           ))
